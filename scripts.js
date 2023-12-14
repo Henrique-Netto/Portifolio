7f9fcdf859a206da705dd9c0a236928642f64868
Henrique Kempin Netto <108898471+Henrique-Netto@users.noreply.github.com>
2023-12-14 11:14:01 -0300

    Correcao pra ficar certo no celular
    Correcao pra ficar certo no celular

--- scripts.js
+++ scripts.js
@@ -4,13 +4,15 @@ $(function(){
         $(this).animate({
             "width": 70,
             "height":70,
-            "margin-top": -30
+            "margin-top": -20
         }, 200);
     }, function(){
         $(this).animate({ //função para ao tirar o mouse voltar ao pradao o icone
             "width": 60,
             "height":60,
-            "margin-top": -0
+            "margin-top": -0,
+            "padding": 0,
+            "border-radius": 8,
         }, 200);
     });
 });
