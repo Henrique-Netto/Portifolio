d45eaa938d35f4f132cdaae24bb8157126bc3b59
Henrique Kempin Netto <108898471+Henrique-Netto@users.noreply.github.com>
2023-12-14 09:53:41 -0300

    Terceiro Commit
    Ajustado novamente as tecnologias e cor dos hover;

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ $(function(){
 
     $('.card').hover(function(){ //função para ao passar o mouse crescer o icone
         $(this).animate({
-            "width": 90,
-            "height":90,
+            "width": 70,
+            "height":70,
             "margin-top": -30
         }, 200);
     }, function(){
         $(this).animate({ //função para ao tirar o mouse voltar ao pradao o icone
-            "width": 80,
-            "height":80,
+            "width": 60,
+            "height":60,
             "margin-top": -0
         }, 200);
     });
